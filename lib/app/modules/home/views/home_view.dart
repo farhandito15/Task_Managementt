92d3d2371042a6ae251e0b990c7c37c78011e0a3
farhandito15 <farhandito15@gmail.com>
2022-07-06 16:49:33 +0700

    commit video 3.1

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -15,188 +15,190 @@ class HomeView extends GetView<HomeController> {
           children: [
             Expanded(
               flex: 2,
-              child: Container(
-                height: Get.height,
-                color: Colors.pink,
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 100,
-                        padding: const EdgeInsets.only(
-                          top: 30,
-                        ),
-                        width: double.infinity,
-                        alignment: Alignment.topCenter,
-                        child: const Image(
-                          image: AssetImage('assets/icons/icon.png'),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 50,
-                      ),
-                      SizedBox(
-                        height: 100,
-                        child: Center(
-                          child: InkWell(
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 40,
-                                  width: 100,
-                                  decoration: Get.currentRoute == '/home'
-                                      ? BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(40),
-                                          color: Colors.white,
-                                        )
-                                      : const BoxDecoration(),
-                                  child: Icon(
-                                    Get.currentRoute == '/home'
-                                        ? Ionicons.desktop
-                                        : Ionicons.desktop_outline,
-                                    color: Colors.grey,
-                                    size: 30,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                const Text(
-                                  'Home',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 16,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            onTap: () => Get.toNamed(Routes.HOME),
+              child: Drawer(
+                child: Container(
+                  height: Get.height,
+                  color: Colors.pink,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 100,
+                          padding: const EdgeInsets.only(
+                            top: 30,
+                          ),
+                          width: double.infinity,
+                          alignment: Alignment.topCenter,
+                          child: const Image(
+                            image: AssetImage('assets/icons/icon.png'),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        child: Center(
-                          child: InkWell(
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 40,
-                                  width: 100,
-                                  decoration: Get.currentRoute == '/task'
-                                      ? BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(40),
-                                          color: Colors.white,
-                                        )
-                                      : const BoxDecoration(),
-                                  child: Icon(
-                                    Get.currentRoute == '/task'
-                                        ? Ionicons.cube
-                                        : Ionicons.cube_outline,
-                                    color: Colors.grey,
-                                    size: 30,
+                        const SizedBox(
+                          height: 50,
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: Get.currentRoute == '/home'
+                                        ? BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(40),
+                                            color: Colors.white,
+                                          )
+                                        : const BoxDecoration(),
+                                    child: Icon(
+                                      Get.currentRoute == '/home'
+                                          ? Ionicons.desktop
+                                          : Ionicons.desktop_outline,
+                                      color: Colors.grey,
+                                      size: 30,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                const Text(
-                                  'Task',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 16,
+                                  const SizedBox(
+                                    height: 5,
                                   ),
-                                ),
-                              ],
+                                  const Text(
+                                    'Home',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onTap: () => Get.toNamed(Routes.HOME),
                             ),
-                            onTap: () => Get.toNamed(Routes.TASK),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        child: Center(
-                          child: InkWell(
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 40,
-                                  width: 100,
-                                  decoration: Get.currentRoute == '/friends'
-                                      ? BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(40),
-                                          color: Colors.white,
-                                        )
-                                      : const BoxDecoration(),
-                                  child: Icon(
-                                    Get.currentRoute == '/friends'
-                                        ? Ionicons.heart
-                                        : Ionicons.heart_outline,
-                                    color: Colors.grey,
-                                    size: 30,
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: Get.currentRoute == '/task'
+                                        ? BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(40),
+                                            color: Colors.white,
+                                          )
+                                        : const BoxDecoration(),
+                                    child: Icon(
+                                      Get.currentRoute == '/task'
+                                          ? Ionicons.cube
+                                          : Ionicons.cube_outline,
+                                      color: Colors.grey,
+                                      size: 30,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                const Text(
-                                  'Friends',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 16,
+                                  const SizedBox(
+                                    height: 5,
                                   ),
-                                ),
-                              ],
+                                  const Text(
+                                    'Task',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onTap: () => Get.toNamed(Routes.TASK),
                             ),
-                            onTap: () => Get.toNamed(Routes.FRIENDS),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 100,
-                        child: Center(
-                          child: InkWell(
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 40,
-                                  width: 100,
-                                  decoration: Get.currentRoute == '/profile'
-                                      ? BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(40),
-                                          color: Colors.white,
-                                        )
-                                      : const BoxDecoration(),
-                                  child: Icon(
-                                    Get.currentRoute == '/profile'
-                                        ? Ionicons.person
-                                        : Ionicons.person_outline,
-                                    color: Colors.grey,
-                                    size: 30,
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: Get.currentRoute == '/friends'
+                                        ? BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(40),
+                                            color: Colors.white,
+                                          )
+                                        : const BoxDecoration(),
+                                    child: Icon(
+                                      Get.currentRoute == '/friends'
+                                          ? Ionicons.heart
+                                          : Ionicons.heart_outline,
+                                      color: Colors.grey,
+                                      size: 30,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                const Text(
-                                  'Profile',
-                                  style: TextStyle(
-                                    color: Colors.grey,
-                                    fontSize: 16,
+                                  const SizedBox(
+                                    height: 5,
                                   ),
-                                ),
-                              ],
+                                  const Text(
+                                    'Friends',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onTap: () => Get.toNamed(Routes.FRIENDS),
                             ),
-                            onTap: () => Get.toNamed(Routes.PROFILE),
                           ),
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          height: 100,
+                          child: Center(
+                            child: InkWell(
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: 40,
+                                    width: 100,
+                                    decoration: Get.currentRoute == '/profile'
+                                        ? BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(40),
+                                            color: Colors.white,
+                                          )
+                                        : const BoxDecoration(),
+                                    child: Icon(
+                                      Get.currentRoute == '/profile'
+                                          ? Ionicons.person
+                                          : Ionicons.person_outline,
+                                      color: Colors.grey,
+                                      size: 30,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  const Text(
+                                    'Profile',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              onTap: () => Get.toNamed(Routes.PROFILE),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
